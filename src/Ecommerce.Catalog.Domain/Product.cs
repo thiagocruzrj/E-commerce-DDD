199c1b7124482c462eedbcfd1489d22061de4a83
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-02 18:17:12 -0300

    Implmenting DomainObject Entity(Superclass)

---
+++ src/Ecommerce.Catalog.Domain/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Catalog.Domain
+{
+    public class Product
+    {
+
+    }
+
+    public class Category
+    {
+    }
+}
---
+++ src/Ecommerce.Core/DomainObjects/Entity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
