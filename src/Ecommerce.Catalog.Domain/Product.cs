fbd31cc42545069306a5f63edfdb31fea9677dc3
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-02 18:47:51 -0300

    Write behaviors on product entity

--- src/Ecommerce.Catalog.Domain/Product.cs
+++ src/Ecommerce.Catalog.Domain/Product.cs
@@ -1,15 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Ecommerce.Core.DomainObjects;
+using System;
 
 namespace Ecommerce.Catalog.Domain
 {
-    public class Product
+    public class Product : Entity, IAggregateRoot
     {
 
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime RegisterDate { get; private set; }
+        public string Image { get; private set; }
+        public int StockQuantity { get; private set; }
+        public Category Category { get; private set; }
+
+        public Product(string name, string description, bool active, decimal value, Guid categoryId, DateTime registerDate, string image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Value = value;
+            RegisterDate = registerDate;
+            Image = image;
+        }
+
+        public void Activate() => Active = true;
+        public void Deactive() => Active = false;
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void DebitStock(int quantity)
+        {
+            if (quantity < 0) quantity *= -1;
+            StockQuantity -= quantity;
+        }
+
+        public void ReplenishStock(int quantity)
+        {
+            StockQuantity += quantity;
+        }
+
+        public bool HasStock(int quantity)
+        {
+            return StockQuantity >= quantity;
+        }
+
+        public void Validate()
+        {
+
+        }
     }
 
-    public class Category
+    public class Category : Entity
     {
+
     }
 }
---
+++ src/Ecommerce.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,4 @@
+﻿namespace Ecommerce.Core.DomainObjects
+{
+    public interface IAggregateRoot { }
+}
