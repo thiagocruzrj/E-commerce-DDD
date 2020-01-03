f1557b6abf46a770cb71d8576f28b9b699e3cdfa
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-03 17:04:37 -0300

    Added Stock Domain Service on Domain Layer

---
+++ src/Ecommerce.Catalog.Domain/DomainService/IStockService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Catalog.Domain.DomainService
+{
+    public interface IStockService : IDisposable
+    {
+        Task<bool> DebitStock(Guid productId, int quantity);
+        Task<bool> ReplenishStock(Guid productId, int quantity);
+    }
+}
---
+++ src/Ecommerce.Catalog.Domain/DomainService/StockService.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Catalog.Domain.Repository;
+using System;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Catalog.Domain.DomainService
+{
+    public class StockService : IStockService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public StockService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> DebitStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetById(productId);
+
+            if (product == null) return false;
+            if (!product.HasStock(quantity)) return false;
+            product.DebitStock(quantity);
+            _productRepository.Update(product);
+
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReplenishStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetById(productId);
+
+            if (product == null) return false;
+            product.ReplenishStock(quantity);
+
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _productRepository.Dispose();
+        }
+    }
+}
