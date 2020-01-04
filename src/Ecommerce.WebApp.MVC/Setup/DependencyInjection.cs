932d6f90b0c8859a83fac3ba6a8bd0f7f19619de
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-04 15:30:19 -0300

    Starting MVC webapp and config DI

---
+++ src/Ecommerce.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Catalog.Application.Services;
+using Ecommerce.Catalog.Data;
+using Ecommerce.Catalog.Data.Repository;
+using Ecommerce.Catalog.Domain.DomainService;
+using Ecommerce.Catalog.Domain.Repository;
+using Ecommerce.Core.Communication;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // Domain Bus (Mediator)
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalog
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductAppService, ProductAppService>();
+            services.AddScoped<IStockService, StockService>();
+            services.AddScoped<CatalogContext>();
+        }
+    }
+}
