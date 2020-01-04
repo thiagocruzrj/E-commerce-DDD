bf911b9029af61a5328b237ebb9376d3d214b98f
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-04 14:46:38 -0300

    Finishing Catalog application layer with automapper profile configuration

--- src/Ecommerce.Catalog.Application/AutoMapper/DomainToViewModelMappingProfile.cs
+++ src/Ecommerce.Catalog.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using Ecommerce.Catalog.Application.ViewModel;
+using Ecommerce.Catalog.Domain.Entities;
 
 namespace Ecommerce.Catalog.Application.AutoMapper
 {
-    interface DomainToViewModelMappingProfile
+    public class DomainToViewModelMappingProfile : Profile
     {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Product, ProductViewModel>()
+                .ForMember(d => d.Height, o => o.MapFrom(s => s.Dimensions.Height))
+                .ForMember(d => d.Width, o => o.MapFrom(s => s.Dimensions.Width))
+                .ForMember(d => d.Depth, o => o.MapFrom(s => s.Dimensions.Depth));
+            CreateMap<Category, CategoryViewModel>();
+        }
     }
 }
--- src/Ecommerce.Catalog.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ src/Ecommerce.Catalog.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using Ecommerce.Catalog.Application.ViewModel;
+using Ecommerce.Catalog.Domain.Entities;
+using Ecommerce.Catalog.Domain.ValueObjects;
 
 namespace Ecommerce.Catalog.Application.AutoMapper
 {
-    class ViewModelToDomainMappingProfile
+    public class ViewModelToDomainMappingProfile : Profile
     {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProductViewModel, Product>()
+                .ConstructUsing(p =>
+                    new Product(p.Name, p.Description, p.Active,
+                        p.Value, p.CategoryId, p.RegisterDate,
+                        p.Image, new Dimensions(p.Height, p.Width, p.Depth)));
+
+            CreateMap<CategoryViewModel, Category>()
+                .ConstructUsing(c => new Category(c.Name, c.Code));
+        }
     }
 }
