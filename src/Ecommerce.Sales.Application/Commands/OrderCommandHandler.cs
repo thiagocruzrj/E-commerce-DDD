35ff1275050a781b4aa4faf48a8fb0b744fbf580
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-09 12:34:36 -0300

    Add first command logic Order handler

--- src/Ecommerce.Sales.Application/Commands/OrderCommandHandler.cs
+++ src/Ecommerce.Sales.Application/Commands/OrderCommandHandler.cs
@@ -1,7 +1,10 @@
 ﻿using Ecommerce.Core.Messages;
+using Ecommerce.Sales.Domain.Entities;
+using Ecommerce.Sales.Domain.Repositories;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,12 +13,40 @@ namespace Ecommerce.Sales.Application.Commands
 {
     public class OrderCommandHandler : IRequestHandler<AddOrderItemCommand, bool>
     {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderCommandHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
         public async Task<bool> Handle(AddOrderItemCommand message, CancellationToken cancellationToken)
         {
             if (!ValidateCommand(message)) return false;
 
-            return true;
-            
+            var order = await _orderRepository.GetDraftOrderByClientId(message.ClientId);
+            var orderItem = new OrderItem(message.ProductId, message.Name, message.Quantity, message.UnitValue);
+
+            if(order == null)
+            {
+                order = Order.OrderFactory.NewOrderDraft(message.ClientId);
+                order.AddItem(orderItem);
+
+                _orderRepository.AddOrder(order);
+            } else
+            {
+                var orderItemExistent = order.OrderItemExist(orderItem);
+                order.AddItem(orderItem);
+
+                if (orderItemExistent)
+                {
+                    _orderRepository.UpdateOrderItem(order.OrderItem.FirstOrDefault(p => p.ProductId == orderItem.ProductId));
+                } else
+                {
+                    _orderRepository.AddOrderItem(orderItem);
+                }
+            }
+            return await _orderRepository.UnitOfWork.Commit();
         }
 
         private bool ValidateCommand(Command message)
--- src/Ecommerce.Sales.Domain/Repositories/IOrderRepository.cs
+++ src/Ecommerce.Sales.Domain/Repositories/IOrderRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace Ecommerce.Sales.Domain.Repositories
+﻿using Ecommerce.Core.Data;
+using Ecommerce.Sales.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Sales.Domain.Repositories
 {
-    public interface IOrderRepository
+    public interface IOrderRepository : IRepository<Order>
     {
+        Task<IEnumerable<Order>> GetListByClientId(Guid clientId);
+        Task<Order> GetDraftOrderByClientId(Guid clientId);
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+
+        Task<OrderItem> GetItemById(Guid id);
+        Task<OrderItem> GetItemByOrder(Guid orderId, Guid productId);
+        void AddOrderItem(OrderItem orderItem);
+        void UpdateOrderItem(OrderItem orderItem);
+        void RemoveOrderItem(OrderItem orderItem);
+        Task<Voucher> GetVoucherByCode(string code);
     }
 }
