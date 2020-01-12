5784f8783e1a93ba7f1eb75cae51aeb0a984631a
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-12 13:36:20 -0300

    Implementing summry error message component

---
+++ src/Ecommerce.WebApp.MVC/Extensions/SummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Core.Messages.CommonMessages.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Ecommerce.WebApp.MVC.Extensions
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        private readonly DomainNotificationHandler _notifications;
+
+        public SummaryViewComponent(INotificationHandler<DomainNotification> notifications)
+        {
+            _notifications = (DomainNotificationHandler)notifications;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notifications = await Task.FromResult(_notifications.GetNotifications());
+            notifications.ForEach(c => ViewData.ModelState.AddModelError(string.Empty, c.Value));
+
+            return View();
+        }
+    }
+}
