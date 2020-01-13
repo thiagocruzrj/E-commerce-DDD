29c100c2482980b51f9cd14357800cd968334202
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-13 18:40:48 -0300

    Implemented Index Cart view and some methods commented on controller

--- src/Ecommerce.WebApp.MVC/Controllers/ShopCartController.cs
+++ src/Ecommerce.WebApp.MVC/Controllers/ShopCartController.cs
@@ -4,6 +4,7 @@ using Ecommerce.Catalog.Application.Services;
 using Ecommerce.Core.Communication.Mediator;
 using Ecommerce.Core.Messages.CommonMessages.Notifications;
 using Ecommerce.Sales.Application.Commands;
+using Ecommerce.Sales.Application.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,15 +13,24 @@ namespace Ecommerce.WebApp.MVC.Controllers
     public class ShopCartController : ControllerBase
     {
         private readonly IProductAppService _productAppService;
+        private readonly IOrderQueries _orderQueries;
         private readonly IMediatrHandler _mediatrHandler;
 
         public ShopCartController(INotificationHandler<DomainNotification> notifications,
                                     IProductAppService productAppService,
+                                    IOrderQueries orderQueries,
                                     IMediatrHandler mediatrHandler)
                                     : base(notifications, mediatrHandler)
         {
             _mediatrHandler = mediatrHandler;
+            _orderQueries = orderQueries;
             _productAppService = productAppService;
+        }
+
+        [Route("my-cart")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _orderQueries.GetCartByClient(ClientId));
         }
 
         [HttpPost]
@@ -47,5 +57,54 @@ namespace Ecommerce.WebApp.MVC.Controllers
             TempData["Erros"] = GetErroMessage();
             return RedirectToAction("ProductDetail", "ShopWindow", new { id });
         }
+
+        //[HttpPost]
+        //[Route("remove-item")]
+        //public async Task<IActionResult> RemoveItem(Guid id)
+        //{
+        //    var product = await _productAppService.GetById(id);
+        //    if (product == null) return BadRequest();
+
+        //    var command = new RemoveOrderItemCommand(ClientId, id);
+        //    await _mediatrHandler.SendCommand(command);
+
+        //    if(ValidOperation())
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View("Index", await _orderQueries.GetCartByClient(ClientId));
+        //}
+
+        //[HttpPost]
+        //[Route("update-item")]
+        //public async Task<IActionResult> UpdateItem(Guid id, int quantity)
+        //{
+        //    var product = await _productAppService.GetById(id);
+        //    if (product == null) return BadRequest();
+
+        //    var command = new UpdateOrderItemCommand(ClientId, id, quantity);
+        //    await _mediatrHandler.SendCommand(command);
+
+        //    if(ValidOperation())
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //}
+
+        //[HttpPost]
+        //[Route("apply-voucher")]
+        //public async Task<IActionResult> ApplyVoucher(string voucherCode)
+        //{
+        //    var command = new ApplyOrderVoucherCommand(ClientId, voucherCode);
+        //    await _mediatrHandler.SendCommand(command);
+
+        //    if(ValidOperation())
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View("Index", await _orderQueries.GetCartByClient(ClientId));
+        //}
     }
 }
