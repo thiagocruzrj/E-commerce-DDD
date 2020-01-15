64482bfb623c6bc01e8ab592839cd01fa115a6ab
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-15 16:24:24 -0300

    Implementing payment context and mediator extensions on data layer

--- src/Ecommerce.Payments.Data/Extensions/MediatorExtension.cs
+++ src/Ecommerce.Payments.Data/Extensions/MediatorExtension.cs
@@ -1,6 +1,31 @@
-﻿namespace Ecommerce.Payments.Data.Extensions
+﻿using Ecommerce.Core.Communication.Mediator;
+using Ecommerce.Core.DomainObjects;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Payments.Data.Extensions
 {
-    public class MediatorExtension
+    public static class MediatorExtension
     {
+        public static async Task PublishEvents(this IMediatrHandler mediator, PaymentContext ctx)
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Notifications != null && x.Entity.Notifications.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notifications)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.CleanEvents());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => {
+                    await mediator.PublishEvent(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
+        }
     }
 }
--- src/Ecommerce.Payments.Data/PaymentContext.cs
+++ src/Ecommerce.Payments.Data/PaymentContext.cs
@@ -1,6 +1,58 @@
-﻿namespace Ecommerce.Payments.Data
+﻿using Ecommerce.Core.Communication.Mediator;
+using Ecommerce.Core.Data;
+using Ecommerce.Core.Messages;
+using Ecommerce.Payments.Business.Entities;
+using Ecommerce.Payments.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Payments.Data
 {
-    public class PaymentContext
+    public class PaymentContext : DbContext, IUnitOfWork
     {
+        private readonly IMediatrHandler _mediatorHandler;
+
+        public PaymentContext(DbContextOptions<PaymentContext> options,IMediatrHandler mediatorHandler) : base(options)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.Ignore<Event>();
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("RegisterDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            var success = await base.SaveChangesAsync() > 0;
+            if (success) await _mediatorHandler.PublishEvents(this);
+
+            return success;
+        }
     }
 }
